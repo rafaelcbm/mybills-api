7531ba6772fae9757c492d2656e7896ab4317d27
Rafael Correia <rafaelcbm@gmail.com>
2021-04-07 10:05:42 -0300

    rafactor: add wallet controller and route

--- src/main/factories/controllers/add-wallet-controller-factory.ts
+++ src/main/factories/controllers/add-wallet-controller-factory.ts
@@ -1,7 +1,7 @@
 import { makeLogControllerDecorator } from '@/main/factories'
 import { AddWalletController } from '@/presentation/controllers'
 import { Controller } from '@/presentation/protocols'
-import { makeDbAddWallet } from '../usecases/add-wallet-factory'
+import { makeDbAddWallet } from '@/main/factories/usecases'
 import { makeAddWalletValidation } from './add-wallet-validation-factory'
 
 export const makeAddWalletController = (): Controller => {
--- src/main/routes/wallet-routes.ts
+++ src/main/routes/wallet-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddWalletController } from '@/main/factories'
+import { makeAddWalletController, makeLoadWalletsController } from '@/main/factories'
 import { auth } from '@/main/middlewares'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/wallets', auth, adaptRoute(makeAddWalletController()))
+  router.get('/wallets', auth, adaptRoute(makeLoadWalletsController()))
 }
