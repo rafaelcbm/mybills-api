cd2c0502affcd53682cbb54ce14593d9e9e9a56e
Rafael Correia <rafaelcbm@gmail.com>
2021-04-06 21:48:36 -0300

    test: load wallets repository

--- tests/infra/db/mongodb/wallet-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/wallet-mongo-repository.spec.ts
@@ -1,12 +1,19 @@
 import { MongoHelper, WalletMongoRepository } from '@/infra/db'
 import { mockAddWalletRepositoryParams } from '@/tests/data/mocks'
+import { mockAddAccountParams } from '@/tests/domain/mocks'
 import { Collection } from 'mongodb'
+
+let walletCollection: Collection
+let accountCollection: Collection
 
 const makeSut = (): WalletMongoRepository => {
   return new WalletMongoRepository()
 }
 
-let walletCollection: Collection
+const mockAccountId = async (): Promise<string> => {
+  const res = await accountCollection.insertOne(mockAddAccountParams())
+  return res.ops[0]._id
+}
 
 describe('WalletMongoRepository', () => {
   beforeAll(async () => {
@@ -20,6 +27,8 @@ describe('WalletMongoRepository', () => {
   beforeEach(async () => {
     walletCollection = await MongoHelper.getCollection('wallets')
     await walletCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('add()', () => {
@@ -28,6 +37,32 @@ describe('WalletMongoRepository', () => {
       await sut.add(mockAddWalletRepositoryParams())
       const count = await walletCollection.countDocuments()
       expect(count).toBe(1)
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all wallets on success', async () => {
+      const accountId = await mockAccountId()
+      const walletsParams = [
+        Object.assign({} , mockAddWalletRepositoryParams(), { accountId }),
+        Object.assign({} , mockAddWalletRepositoryParams(), { accountId })]
+      await walletCollection.insertMany(walletsParams)
+      const sut = makeSut()
+      const wallets = await sut.loadAll(accountId)
+      expect(wallets.length).toBe(2)
+      expect(wallets[0].accountId).toBeTruthy()
+      expect(wallets[0].accountId).toEqual(accountId)
+      expect(wallets[0].name).toBeTruthy()
+      expect(wallets[1].accountId).toBeTruthy()
+      expect(wallets[1].accountId).toEqual(accountId)
+      expect(wallets[1].name).toBeTruthy()
+    })
+
+    test('Should load empty list', async () => {
+      const accountId = await mockAccountId()
+      const sut = makeSut()
+      const wallets = await sut.loadAll(accountId)
+      expect(wallets.length).toBe(0)
     })
   })
 })
