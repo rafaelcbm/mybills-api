04900689081367db9ea7d8b10a7ad95476f554fe
Rafael Correia <rafaelcbm@gmail.com>
2021-04-06 21:48:25 -0300

    feat: load wallets repository

--- src/infra/db/mongodb/walllet-mongo-repository.ts
+++ src/infra/db/mongodb/walllet-mongo-repository.ts
@@ -1,9 +1,15 @@
-import { AddWalletRepository, AddWalletRepositoryParams } from '@/data/protocols/db'
+import { AddWalletRepository, AddWalletRepositoryParams, LoadWalletsRepository, LoadWalletRepositoryResult } from '@/data/protocols/db'
 import { MongoHelper } from '@/infra/db'
 
-export class WalletMongoRepository implements AddWalletRepository {
+export class WalletMongoRepository implements AddWalletRepository, LoadWalletsRepository {
   async add (walletParam: AddWalletRepositoryParams): Promise<void> {
     const walletCollection = await MongoHelper.getCollection('wallets')
     await walletCollection.insertOne(walletParam)
+  }
+
+  async loadAll (accountId: string): Promise<LoadWalletRepositoryResult[]> {
+    const walletCollection = await MongoHelper.getCollection('wallets')
+    const wallets = await walletCollection.find({ accountId }).toArray()
+    return MongoHelper.mapCollection(wallets)
   }
 }
