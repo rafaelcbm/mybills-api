bf148758ce8a6caba209c656462d675003432761
Rafael Correia <rafaelcbm@gmail.com>
2021-04-07 22:20:31 -0300

    test: remove wallet repository

--- tests/infra/db/mongodb/wallet-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/wallet-mongo-repository.spec.ts
@@ -2,6 +2,7 @@ import { MongoHelper, WalletMongoRepository } from '@/infra/db'
 import { mockAddWalletRepositoryParams } from '@/tests/data/mocks'
 import { mockAddAccountParams } from '@/tests/domain/mocks'
 import { Collection } from 'mongodb'
+import faker from 'faker'
 
 let walletCollection: Collection
 let accountCollection: Collection
@@ -63,6 +64,43 @@ describe('WalletMongoRepository', () => {
       const sut = makeSut()
       const wallets = await sut.loadAll(accountId)
       expect(wallets.length).toBe(0)
+    })
+  })
+
+  describe('remove()', () => {
+    test('Should remove a wallet on success', async () => {
+      const accountId = await mockAccountId()
+      const wallet1 = mockAddWalletRepositoryParams()
+      const wallet2 = mockAddWalletRepositoryParams()
+      const walletsParams = [
+        Object.assign({} , wallet1, { accountId }),
+        Object.assign({} , wallet2, { accountId })]
+
+      const insertedWalletsResult = await walletCollection.insertMany(walletsParams)
+      expect(insertedWalletsResult.ops.length).toBe(2)
+      const dbWallet1 = insertedWalletsResult.ops[0]
+
+      const sut = makeSut()
+      const removedWalllet = await sut.remove(dbWallet1._id, accountId)
+      expect(removedWalllet).toBeTruthy()
+      expect(removedWalllet.accountId).toEqual(dbWallet1.accountId)
+      expect(removedWalllet.id).toEqual(dbWallet1._id)
+      expect(removedWalllet.name).toEqual(dbWallet1.name)
+    })
+
+    test('Should return undefined with could not find the wallet', async () => {
+      const accountId = await mockAccountId()
+      const wallet1 = mockAddWalletRepositoryParams()
+      const wallet2 = mockAddWalletRepositoryParams()
+      const walletsParams = [
+        Object.assign({} , wallet1, { accountId }),
+        Object.assign({} , wallet2, { accountId })]
+      const insertedWalletsResult = await walletCollection.insertMany(walletsParams)
+      expect(insertedWalletsResult.ops.length).toBe(2)
+
+      const sut = makeSut()
+      const removedWalllet = await sut.remove(faker.random.word(), accountId)
+      expect(removedWalllet).toBeUndefined()
     })
   })
 })
