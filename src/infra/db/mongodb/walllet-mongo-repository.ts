b851988a9c252608bf5d5733e9afe6a7d7c5a284
Rafael Correia <rafaelcbm@gmail.com>
2021-04-09 15:14:50 -0300

    feat: update wallet mongo repository

--- src/infra/db/mongodb/walllet-mongo-repository.ts
+++ src/infra/db/mongodb/walllet-mongo-repository.ts
@@ -1,9 +1,9 @@
-import { AddWalletRepository, AddWalletRepositoryParams, LoadWalletRepositoryResult, LoadWalletsRepository, RemoveWalletRepository } from '@/data/protocols/db'
+import { AddWalletRepository, AddWalletRepositoryParams, LoadWalletRepositoryResult, LoadWalletsRepository, RemoveWalletRepository, UpdateWalletRepository, UpdateWalletRepositoryParams, UpdateWalletRepositoryResult } from '@/data/protocols/db'
 import { WalletModel } from '@/domain/models'
 import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'mongodb'
 
-export class WalletMongoRepository implements AddWalletRepository, LoadWalletsRepository, RemoveWalletRepository {
+export class WalletMongoRepository implements AddWalletRepository, LoadWalletsRepository, RemoveWalletRepository, UpdateWalletRepository {
   async add (walletParam: AddWalletRepositoryParams): Promise<void> {
     const walletCollection = await MongoHelper.getCollection('wallets')
     await walletCollection.insertOne(walletParam)
@@ -21,6 +21,25 @@ export class WalletMongoRepository implements AddWalletRepository, LoadWalletsRe
 
     if (removedWallet.ok && removedWallet.value) {
       return MongoHelper.map(removedWallet.value)
+    }
+  }
+
+  async update (updateWalletRepositoryParams: UpdateWalletRepositoryParams): Promise<UpdateWalletRepositoryResult> {
+    const walletCollection = await MongoHelper.getCollection('wallets')
+    const updatedWallet = await walletCollection.findOneAndUpdate(
+      {
+        _id: new ObjectId(updateWalletRepositoryParams.id),
+        accountId: new ObjectId(updateWalletRepositoryParams.accountId)
+      }, {
+        $set: {
+          name: updateWalletRepositoryParams.name
+        }
+      }, {
+        returnOriginal: false
+      })
+
+    if (updatedWallet.ok && updatedWallet.value) {
+      return MongoHelper.map(updatedWallet.value)
     }
   }
 }
