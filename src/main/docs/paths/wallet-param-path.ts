a187a23d8707ba275e09819179efa316a646e5aa
Rafael Correia <rafaelcbm@gmail.com>
2021-04-12 10:13:40 -0300

    docs: update wallet api docs

--- src/main/docs/paths/wallet-param-path.ts
+++ src/main/docs/paths/wallet-param-path.ts
@@ -32,5 +32,49 @@ export const walletParamPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  put: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Wallets'],
+    summary: 'API to update an user wallet',
+    description: 'This route must be accessible by **authenticated users**',
+    parameters: [{
+      in: 'path',
+      name: 'walletId',
+      description: 'Wallet ID to be update',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/addWalletParams'
+          }
+        }
+      }
+    },
+    responses: {
+      204: {
+        description: 'Success, no content returned'
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
