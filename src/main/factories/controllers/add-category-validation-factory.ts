9bdea0caf69682c4f6a91737689fd947c0804f3a
Rafael Correia <rafaelcbm@gmail.com>
2021-04-19 21:02:00 -0300

    refactor: required fields changed on add category

--- src/main/factories/controllers/add-category-validation-factory.ts
+++ src/main/factories/controllers/add-category-validation-factory.ts
@@ -3,7 +3,7 @@ import { RequiredFieldValidation, ValidationComposite } from '@/validation/valid
 
 export const makeAddCategoryValidation = (): ValidationComposite => {
   const validations: Validation[] = []
-  for (const field of ['name', 'root', 'ancestors']) {
+  for (const field of ['name']) {
     validations.push(new RequiredFieldValidation(field))
   }
   return new ValidationComposite(validations)
