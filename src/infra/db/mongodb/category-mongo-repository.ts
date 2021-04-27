ac6b4c6c48994c310432eaa8d0a8f19a46e3f9ce
Rafael Correia <rafaelcbm@gmail.com>
2021-04-26 21:50:25 -0300

    feat: remove category mongo repository

--- src/infra/db/mongodb/category-mongo-repository.ts
+++ src/infra/db/mongodb/category-mongo-repository.ts
@@ -1,8 +1,11 @@
-import { AddCategoryRepository, AddCategoryRepositoryParams, LoadCategoriesRepository, LoadCategoriesRepositoryResult } from '@/data/protocols/db'
+import { AddCategoryRepository, AddCategoryRepositoryParams, LoadCategoriesRepository, LoadCategoriesRepositoryResult, RemoveCategoryRepository } from '@/data/protocols/db'
+import { LoadChildCategoriesRepository } from '@/data/protocols/db/category/load-child-categories-repository'
+import { RemoveChildCategoriesRepository } from '@/data/protocols/db/category/remove-child-categories-repository'
 import { CategoryModel } from '@/domain/models'
 import { MongoHelper } from '@/infra/db'
+import { ObjectId } from 'bson'
 
-export class CategoryMongoRepository implements AddCategoryRepository, LoadCategoriesRepository {
+export class CategoryMongoRepository implements AddCategoryRepository, LoadCategoriesRepository, RemoveCategoryRepository , LoadChildCategoriesRepository , RemoveChildCategoriesRepository {
   async add (categoryParam: AddCategoryRepositoryParams): Promise<CategoryModel> {
     const categoryCollection = await MongoHelper.getCollection('categories')
     const category = await categoryCollection.insertOne(categoryParam)
@@ -14,5 +17,27 @@ export class CategoryMongoRepository implements AddCategoryRepository, LoadCateg
     const categories = await categoryCollection
       .find({ accountId },{ projection: { accountId: 0 } }).toArray()
     return MongoHelper.mapCollection(categories)
+  }
+
+  async remove (accountId: string, categoryId: string): Promise<CategoryModel> {
+    const categoriesCollection = await MongoHelper.getCollection('categories')
+    const removedCategory = await categoriesCollection.findOneAndDelete({ _id: new ObjectId(categoryId), accountId })
+
+    if (removedCategory.ok && removedCategory.value) {
+      return MongoHelper.map(removedCategory.value)
+    }
+  }
+
+  async loadChild (accountId: string, root: string): Promise<CategoryModel[]> {
+    const categoryCollection = await MongoHelper.getCollection('categories')
+    const categories = await categoryCollection
+      .find({ accountId, root },{ projection: { accountId: 0 } }).toArray()
+    return MongoHelper.mapCollection(categories)
+  }
+
+  async removeChildCategories (accountId: string, root: string): Promise<number> {
+    const categoryCollection = await MongoHelper.getCollection('categories')
+    const deleteResult = await categoryCollection.deleteMany({ accountId, ancestors: root })
+    return deleteResult.deletedCount
   }
 }
