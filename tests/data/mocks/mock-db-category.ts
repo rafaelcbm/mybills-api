398d465883eea6a11e528699ef1a8b0a13316a1e
Rafael Correia <rafaelcbm@gmail.com>
2021-04-26 21:18:04 -0300

    test: remove category use case

--- tests/data/mocks/mock-db-category.ts
+++ tests/data/mocks/mock-db-category.ts
@@ -1,6 +1,8 @@
-import { AddCategoryRepository, AddCategoryRepositoryParams, AddCategoryRepositoryResult, LoadCategoriesRepository, LoadCategoriesRepositoryResult } from '@/data/protocols'
+import { AddCategoryRepository, AddCategoryRepositoryParams, AddCategoryRepositoryResult, LoadCategoriesRepository, LoadCategoriesRepositoryResult, RemoveCategoryRepository, RemoveCategoryRepositoryResult } from '@/data/protocols'
 import { AddCategoryParams } from '@/domain/usecases'
 import faker from 'faker'
+import { LoadChildCategoriesRepository, LoadChildCategoriesRepositoryResult } from '../protocols/db/category/load-child-categories-repository'
+import { RemoveChildCategoriesRepository } from '../protocols/db/category/remove-child-categories-repository'
 
 export const mockAddCategoryRepository = (): AddCategoryRepository => {
   class AddCategoryRepositoryStub implements AddCategoryRepository {
@@ -33,18 +35,65 @@ export const mockAddCategoryRepositoryResult = (accountId?: string): AddCategory
   }
 }
 
-export const mockLoadCategoriesRepository = (): LoadCategoriesRepository => {
+export const mockLoadCategoriesRepository = (categoryId?: string): LoadCategoriesRepository => {
   class LoadCategoryRepositoryStub implements LoadCategoriesRepository {
     async loadAll (accountId: string): Promise<LoadCategoriesRepositoryResult[]> {
-      return mockLoadCategoryRepositoryResult()
+      return mockLoadCategoryRepositoryResult(categoryId)
     }
   }
   return new LoadCategoryRepositoryStub()
 }
 
-export const mockLoadCategoryRepositoryResult = (): LoadCategoriesRepositoryResult[] => {
+export const mockLoadCategoryRepositoryResult = (categoryId?: string): LoadCategoriesRepositoryResult[] => {
+  return [{
+    id: categoryId || faker.random.word(),
+    name: faker.random.words(),
+    ancestors: [
+      faker.random.word(), faker.random.word()
+    ],
+    root: faker.random.word()
+  },
+  {
+    id: categoryId || faker.random.word(),
+    name: faker.random.words(),
+    ancestors: [
+      faker.random.word(), faker.random.word()
+    ],
+    root: faker.random.word()
+  }]
+}
+
+export const mockRemoveCategoryRepository = (): RemoveCategoryRepository => {
+  class RemoveCategoryRepositoryStub implements RemoveCategoryRepository {
+    async remove (categoryId: string, accountId: string): Promise<RemoveCategoryRepositoryResult> {
+      return Promise.resolve(mockRemoveCategoryRepositoryResult())
+    }
+  }
+  return new RemoveCategoryRepositoryStub()
+}
+
+export const mockRemoveChildCategoriesRepository = (): RemoveChildCategoriesRepository => {
+  class RemoveChildCategoriesRepositoryStub implements RemoveChildCategoriesRepository {
+    async removeChildCategories (root: string, accountId: string): Promise<number> {
+      return Promise.resolve(2)
+    }
+  }
+  return new RemoveChildCategoriesRepositoryStub()
+}
+
+export const mockLoadChildCategoriesRepository = (): LoadChildCategoriesRepository => {
+  class LoadChildCategoriesRepositoryStub implements LoadChildCategoriesRepository {
+    async loadChild (root: string, accountId: string): Promise<LoadChildCategoriesRepositoryResult[]> {
+      return mockLoadChildCategoriesRepositoryResult()
+    }
+  }
+  return new LoadChildCategoriesRepositoryStub()
+}
+
+export const mockLoadChildCategoriesRepositoryResult = (): LoadChildCategoriesRepositoryResult[] => {
   return [{
     id: faker.random.word(),
+    accountId: faker.random.word(),
     name: faker.random.words(),
     ancestors: [
       faker.random.word(), faker.random.word()
@@ -53,10 +102,23 @@ export const mockLoadCategoryRepositoryResult = (): LoadCategoriesRepositoryResu
   },
   {
     id: faker.random.word(),
+    accountId: faker.random.word(),
     name: faker.random.words(),
     ancestors: [
       faker.random.word(), faker.random.word()
     ],
     root: faker.random.word()
   }]
+}
+
+export const mockRemoveCategoryRepositoryResult = (): RemoveCategoryRepositoryResult => {
+  return {
+    id: faker.random.word(),
+    accountId: faker.random.word(),
+    name: faker.random.words(),
+    ancestors: [
+      faker.random.word(), faker.random.word()
+    ],
+    root: faker.random.word()
+  }
 }
