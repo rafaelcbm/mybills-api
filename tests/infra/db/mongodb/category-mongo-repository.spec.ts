18afbbf43cc6055036901419e6b87b90076ff542
Rafael Correia <rafaelcbm@gmail.com>
2021-04-28 21:17:10 -0300

    test: category mongo repository

--- tests/infra/db/mongodb/category-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/category-mongo-repository.spec.ts
@@ -70,4 +70,107 @@ describe('CategoryMongoRepository', () => {
       expect(categories.length).toBe(0)
     })
   })
+
+  describe('remove()', () => {
+    test('Should remove a category on success', async () => {
+      const accountId = await mockAccountId()
+      const category1 = mockAddCategoryRepositoryParams(accountId)
+      const category2 = mockAddCategoryRepositoryParams(accountId)
+      const categorysParams = [category1, category2]
+
+      const insertedCategoriesResult = await categoriesCollection.insertMany(categorysParams)
+      expect(insertedCategoriesResult.ops.length).toBe(2)
+      const dbCategory1 = insertedCategoriesResult.ops[0]
+
+      const sut = makeSut()
+      const removedCategory = await sut.remove(accountId, dbCategory1._id)
+
+      expect(removedCategory).toBeTruthy()
+      expect(removedCategory.accountId).toEqual(dbCategory1.accountId)
+      expect(removedCategory.id).toEqual(dbCategory1._id)
+      expect(removedCategory.root).toEqual(dbCategory1.root)
+      expect(removedCategory.name).toEqual(dbCategory1.name)
+      expect(removedCategory.ancestors).toEqual(dbCategory1.ancestors)
+    })
+  })
+
+  describe('loadChild()', () => {
+    test('Should load category children on success', async () => {
+      const accountId = await mockAccountId()
+      const category1 = mockAddCategoryRepositoryParams(accountId)
+      const category2 = mockAddCategoryRepositoryParams(accountId)
+      const category3 = mockAddCategoryRepositoryParams(accountId)
+
+      const rootCategoryResult = await categoriesCollection.insertOne(category1)
+      const rootCategory = rootCategoryResult.ops[0]
+
+      category2.root = rootCategory.name
+      const childCategory2Result = await categoriesCollection.insertOne(category2)
+      const childCategory2 = childCategory2Result.ops[0]
+      expect(childCategory2).toBeTruthy()
+
+      category3.root = rootCategory.name
+      const childCategory3Result = await categoriesCollection.insertOne(category3)
+      const childCategory3 = childCategory3Result.ops[0]
+      expect(childCategory3).toBeTruthy()
+
+      const sut = makeSut()
+      const childCategories = await sut.loadChild(accountId, rootCategory.name)
+
+      expect(childCategories.length).toBe(2)
+      expect(childCategories[0]).not.toHaveProperty('accountId')
+      expect(childCategories[0].name).toBeTruthy()
+      expect(childCategories[0].root).toBe(rootCategory.name)
+      expect(childCategories[0].ancestors.length).toBe(2)
+      expect(childCategories[1]).not.toHaveProperty('accountId')
+      expect(childCategories[1].name).toBeTruthy()
+      expect(childCategories[1].root).toBe(rootCategory.name)
+      expect(childCategories[1].ancestors.length).toBe(2)
+    })
+
+    test('Should load empty list', async () => {
+      const accountId = await mockAccountId()
+      const sut = makeSut()
+      const categories = await sut.loadAll(accountId)
+
+      expect(categories.length).toBe(0)
+    })
+  })
+
+  describe('removeChildCategories()', () => {
+    test('Should remove category children, in all levels, on success', async () => {
+      const accountId = await mockAccountId()
+      const category1 = mockAddCategoryRepositoryParams(accountId)
+      const category2 = mockAddCategoryRepositoryParams(accountId)
+      const category3 = mockAddCategoryRepositoryParams(accountId)
+
+      const rootCategoryResult = await categoriesCollection.insertOne(category1)
+      const rootCategory = rootCategoryResult.ops[0]
+
+      category2.root = rootCategory.name
+      category2.ancestors.push(rootCategory.name)
+      const childCategory2Result = await categoriesCollection.insertOne(category2)
+      const childCategory2 = childCategory2Result.ops[0]
+      expect(childCategory2).toBeTruthy()
+
+      category3.root = childCategory2.name
+      category3.ancestors.push(rootCategory.name , childCategory2.name)
+      const childCategory3Result = await categoriesCollection.insertOne(category3)
+      const childCategory3 = childCategory3Result.ops[0]
+      expect(childCategory3).toBeTruthy()
+
+      const sut = makeSut()
+      const numberCategoriesRemoved = await sut.removeChildCategories(accountId, rootCategory.name)
+
+      expect(numberCategoriesRemoved).toBe(2)
+    })
+
+    test('Should load empty list', async () => {
+      const accountId = await mockAccountId()
+      const sut = makeSut()
+      const categories = await sut.loadAll(accountId)
+
+      expect(categories.length).toBe(0)
+    })
+  })
 })
