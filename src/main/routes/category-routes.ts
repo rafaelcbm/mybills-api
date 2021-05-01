e567017cf01741e5de3f98c00e1556cc6208c546
Rafael Correia <rafaelcbm@gmail.com>
2021-05-01 16:41:05 -0300

    fix:build fix

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -1,10 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAddCategoryController, makeLoadCategoriesController } from '@/main/factories'
+import { makeAddCategoryController, makeLoadCategoriesController, makeRemoveCategoryController } from '@/main/factories'
 import { auth } from '@/main/middlewares'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/categories', auth, adaptRoute(makeLoadCategoriesController()))
   router.post('/categories', auth, adaptRoute(makeAddCategoryController()))
-  router.delete('/categories/:categoryId', auth, adaptRoute(makeRemoveCategoryControll))
+  router.delete('/categories/:categoryId', auth, adaptRoute(makeRemoveCategoryController()))
 }
