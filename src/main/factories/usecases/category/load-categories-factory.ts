088b554468483743963bfc9d7ea1dce99710d90d
Rafael Correia <rafaelcbm@gmail.com>
2021-05-13 10:05:28 -0300

    fix:typo fix

--- src/main/factories/usecases/category/load-categories-factory.ts
+++ src/main/factories/usecases/category/load-categories-factory.ts
@@ -3,6 +3,6 @@ import { LoadCategories } from '@/domain/usecases'
 import { CategoryMongoRepository } from '@/infra/db'
 
 export const makeDbLoadCategories = (): LoadCategories => {
-  const walletMongoRepository = new CategoryMongoRepository()
-  return new DbLoadCategories(walletMongoRepository)
+  const categoryMongoRepository = new CategoryMongoRepository()
+  return new DbLoadCategories(categoryMongoRepository)
 }
