4a84ca85d657499fffed86f459af13cd2ab7e031
Rafael Correia <rafaelcbm@gmail.com>
2021-05-13 10:04:45 -0300

    feat: load root categories mongo implementation

--- src/infra/db/mongodb/category-mongo-repository.ts
+++ src/infra/db/mongodb/category-mongo-repository.ts
@@ -1,4 +1,4 @@
-import { AddCategoryRepository, AddCategoryRepositoryParams, LoadCategoriesRepository, LoadCategoriesRepositoryResult, RemoveCategoryRepository, UpdateCategoryRepository, UpdateCategoryRepositoryParams, UpdateCategoryRepositoryResult } from '@/data/protocols/db'
+import { AddCategoryRepository, AddCategoryRepositoryParams, LoadCategoriesRepository, LoadCategoriesRepositoryResult, LoadRootCategoriesRepository, LoadRootCategoriesRepositoryResult, RemoveCategoryRepository, UpdateCategoryRepository, UpdateCategoryRepositoryParams, UpdateCategoryRepositoryResult } from '@/data/protocols/db'
 import { LoadChildCategoriesRepository } from '@/data/protocols/db/category/load-child-categories-repository'
 import { RemoveChildCategoriesRepository } from '@/data/protocols/db/category/remove-child-categories-repository'
 import { CategoryModel } from '@/domain/models'
@@ -6,7 +6,7 @@ import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'bson'
 
 export class CategoryMongoRepository implements AddCategoryRepository, LoadCategoriesRepository, RemoveCategoryRepository,
-                        LoadChildCategoriesRepository , RemoveChildCategoriesRepository, UpdateCategoryRepository {
+                        LoadChildCategoriesRepository , RemoveChildCategoriesRepository, UpdateCategoryRepository, LoadRootCategoriesRepository {
   async add (categoryParam: AddCategoryRepositoryParams): Promise<CategoryModel> {
     const categoryCollection = await MongoHelper.getCollection('categories')
     const category = await categoryCollection.insertOne(categoryParam)
@@ -33,6 +33,13 @@ export class CategoryMongoRepository implements AddCategoryRepository, LoadCateg
     const categoryCollection = await MongoHelper.getCollection('categories')
     const categories = await categoryCollection
       .find({ accountId, root },{ projection: { accountId: 0 } }).toArray()
+    return MongoHelper.mapCollection(categories)
+  }
+
+  async loadRoots (accountId: string): Promise<LoadRootCategoriesRepositoryResult[]> {
+    const categoryCollection = await MongoHelper.getCollection('categories')
+    const categories = await categoryCollection
+      .find({ accountId, root: null },{ projection: { accountId: 0 } }).toArray()
     return MongoHelper.mapCollection(categories)
   }
 
