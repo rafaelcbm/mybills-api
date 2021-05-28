2fa930eade47676853f56e8bf84c74e4a857e2e2
Rafael Correia <rafaelcbm@gmail.com>
2021-05-28 17:10:25 -0300

    feat: load bill by month mongo repo

--- src/infra/db/mongodb/bill-mongo-repository.ts
+++ src/infra/db/mongodb/bill-mongo-repository.ts
@@ -1,8 +1,11 @@
-import { AddBillRepository, AddBillRepositoryParams, AddBillRepositoryResult, AddManyBillsRepository } from '@/data/protocols/db'
+import { AddBillRepository, AddBillRepositoryParams, AddBillRepositoryResult, AddManyBillsRepository, LoadBillsByMonthParamsParams, LoadBillsByMonthParamsResult, LoadBillsByMonthRepository } from '@/data/protocols/db'
 import { AddBillParams } from '@/domain/usecases'
 import { MongoHelper } from '@/infra/db'
+import { ObjectId } from 'bson'
+import addMonths from 'date-fns/addMonths'
+import parse from 'date-fns/parse'
 
-export class BillMongoRepository implements AddBillRepository, AddManyBillsRepository {
+export class BillMongoRepository implements AddBillRepository, AddManyBillsRepository, LoadBillsByMonthRepository {
   async add (billParam: AddBillRepositoryParams): Promise<AddBillRepositoryResult> {
     const billCollection = await MongoHelper.getCollection('bills')
     const bill = await billCollection.insertOne(billParam)
@@ -12,5 +15,20 @@ export class BillMongoRepository implements AddBillRepository, AddManyBillsRepos
   async addMany (billsParam: AddBillParams[]): Promise<void> {
     const billCollection = await MongoHelper.getCollection('bills')
     await billCollection.insertMany(billsParam)
+  }
+
+  async loadBills (params: LoadBillsByMonthParamsParams): Promise<LoadBillsByMonthParamsResult[]> {
+    const billCollection = await MongoHelper.getCollection('bills')
+
+    const pattern = 'YYYY-MM'
+    const dateReference = new Date(2000, 1, 1)
+
+    const startDate = parse(params.yearMonth, pattern, dateReference)
+    const endDate = addMonths(startDate, 1)
+    const query = { $and: [{ accountId: new ObjectId(params.accountId) }, { date: { $gte: startDate } }, { date: { $lt: endDate } }] }
+
+    const sort = { date: 1 }
+
+    return await billCollection.find(query).sort(sort).toArray()
   }
 }
