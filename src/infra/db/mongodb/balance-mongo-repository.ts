a109fd1842b710b41a194cf51773c393ce74e14c
Rafael Correia <rafaelcbm@gmail.com>
2021-06-30 18:21:18 -0300

    feat: update balance repository

--- src/infra/db/mongodb/balance-mongo-repository.ts
+++ src/infra/db/mongodb/balance-mongo-repository.ts
@@ -1,7 +1,9 @@
-import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult } from '@/data/protocols/db'
+import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult, UpdateBalanceRepository } from '@/data/protocols/db'
+import { BalanceModel } from '@/domain/models'
 import { MongoHelper } from '@/infra/db'
+import { ObjectId } from 'mongodb'
 
-export class BalanceMongoRepository implements LoadBalanceByMonthRepository, AddBalanceRepository {
+export class BalanceMongoRepository implements LoadBalanceByMonthRepository, AddBalanceRepository, UpdateBalanceRepository {
   async loadBalance (params: LoadBalanceByMonthRepositoryParams): Promise<LoadBalanceByMonthRepositoryResult> {
     const billCollection = await MongoHelper.getCollection('balances')
 
@@ -15,5 +17,23 @@ export class BalanceMongoRepository implements LoadBalanceByMonthRepository, Add
   async add (balanceParam: AddBalanceRepositoryParams): Promise<void> {
     const balanceCollection = await MongoHelper.getCollection('balances')
     await balanceCollection.insertOne(balanceParam)
+  }
+
+  async update (balanceId: string, balanceValue: number): Promise<BalanceModel> {
+    const balanceCollection = await MongoHelper.getCollection('balances')
+    const updatedBalance = await balanceCollection.findOneAndUpdate(
+      {
+        _id: new ObjectId(balanceId)
+      }, {
+        $set: {
+          balance: balanceValue
+        }
+      }, {
+        returnOriginal: false
+      })
+
+    if (updatedBalance.ok && updatedBalance.value) {
+      return MongoHelper.map(updatedBalance.value)
+    }
   }
 }
