f3e4686d3c95c043d71e9826b9c622d20605b538
Rafael Correia <rafaelcbm@gmail.com>
2021-06-30 17:44:55 -0300

    test: update balance use case

--- tests/data/mocks/mock-db-balance.ts
+++ tests/data/mocks/mock-db-balance.ts
@@ -1,4 +1,5 @@
-import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult } from '@/data/protocols'
+import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult, UpdateBalanceRepository } from '@/data/protocols'
+import { BalanceModel } from '@/domain/models'
 import { AddBalanceParams } from '@/domain/usecases'
 import faker from 'faker'
 
@@ -37,6 +38,24 @@ export const mockAddBalanceRepository = (): AddBalanceRepository => {
 
 export const mockAddBalanceRepositoryParams = (accountId?: string): AddBalanceRepositoryParams => {
   return {
+    accountId: accountId || faker.random.word(),
+    yearMonth: faker.random.word(),
+    balance: faker.random.number()
+  }
+}
+
+export const mockUpdateBalanceRepository = (): UpdateBalanceRepository => {
+  class UpdateBalanceRepositoryStub implements UpdateBalanceRepository {
+    async update (accountId: string,yearMonth: string, balanceValue: number): Promise<BalanceModel> {
+      return Promise.resolve(mockUpdateBalanceRepositoryResult(accountId))
+    }
+  }
+  return new UpdateBalanceRepositoryStub()
+}
+
+export const mockUpdateBalanceRepositoryResult = (accountId: string): BalanceModel => {
+  return {
+    id: faker.random.word(),
     accountId: accountId || faker.random.word(),
     yearMonth: faker.random.word(),
     balance: faker.random.number()
---
+++ tests/data/usecases/balance/db-update-balance.spec.ts
@@ -0,0 +1,58 @@
+import { UpdateBalanceRepository } from '@/data/protocols'
+import { DbUpdateBalance } from '@/data/usecases'
+import { GenericBusinessError } from '@/domain/errors'
+import { WALLET_NOT_FOUND } from '@/domain/errors/messages/error-messages'
+import { mockBalanceModel, throwError } from '@/tests/domain/mocks'
+import { mockUpdateBalanceRepository } from '@/tests/data/mocks'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: DbUpdateBalance
+  updateBalanceRepositoryStub: UpdateBalanceRepository
+}
+
+const makeSut = (): SutTypes => {
+  const updateBalanceRepositoryStub = mockUpdateBalanceRepository()
+  const sut = new DbUpdateBalance(updateBalanceRepositoryStub)
+  return {
+    sut,
+    updateBalanceRepositoryStub
+  }
+}
+
+describe('DbUpdateBalance Usecase', () => {
+  test('should call UpdateBalanceRepository with correct values ', async () => {
+    const { sut, updateBalanceRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(updateBalanceRepositoryStub, 'update')
+    const accountId = faker.random.word()
+    const yearMonth = faker.random.word()
+    const balance = faker.random.number()
+    await sut.update(accountId, yearMonth, balance)
+    expect(updateSpy).toHaveBeenCalledWith(accountId, yearMonth, balance)
+  })
+
+  test('should return a valid balance if UpdateBalanceRepository returns', async () => {
+    const { sut } = makeSut()
+    const accountId = faker.random.word()
+    const yearMonth = faker.random.word()
+    const balance = faker.random.number()
+    const updatedBalance = await sut.update(accountId, yearMonth, balance)
+    expect(updatedBalance).toBeTruthy()
+    expect(updatedBalance.id).toBeTruthy()
+    expect(updatedBalance.accountId).toBeTruthy()
+    expect(updatedBalance.yearMonth).toBeTruthy()
+    expect(updatedBalance.balance).toBeTruthy()
+  })
+
+  test('Should throw if UpdateBalanceRepository throws', async () => {
+    const { sut, updateBalanceRepositoryStub } = makeSut()
+    jest.spyOn(updateBalanceRepositoryStub, 'update').mockImplementationOnce(throwError)
+
+    const accountId = faker.random.word()
+    const yearMonth = faker.random.word()
+    const balance = faker.random.number()
+
+    const promise = sut.update(accountId, yearMonth, balance)
+    await expect(promise).rejects.toThrow()
+  })
+})
