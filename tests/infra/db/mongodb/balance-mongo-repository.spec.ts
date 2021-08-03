e042a434a70628cf7fde6b5f93da5726cc933491
Rafael Correia <rafaelcbm@gmail.com>
2021-08-03 14:33:19 -0300

    test: load last and load future balance impl

--- tests/infra/db/mongodb/balance-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/balance-mongo-repository.spec.ts
@@ -88,7 +88,6 @@ describe('BalanceMongoRepository', () => {
       const insertBalanceResult = await balancesCollection.insertOne(balance)
       const insertedBalance = insertBalanceResult.ops[0]
       expect(insertedBalance._id).toBeTruthy()
-      console.log('insertedBalance._id = ',insertedBalance._id)
 
       const newBalanceValue = faker.random.number()
       const sut = makeSut()
@@ -106,6 +105,127 @@ describe('BalanceMongoRepository', () => {
       const updatedBalance = await sut.update(FakeObjectId.generate(),faker.random.number())
 
       expect(updatedBalance).toBeUndefined()
+    })
+  })
+
+  describe('loadLastBalance()', () => {
+    test('Should load the last balance from an existing month parameter', async () => {
+      const sut = makeSut()
+      const accountId = await mockAccountId()
+      const balance1 = {
+        accountId,
+        yearMonth: '2021-05',
+        balance: 123.45
+      }
+      const balance2 = {
+        accountId,
+        yearMonth: '2021-06',
+        balance: 321.22
+      }
+      const balance3 = {
+        accountId,
+        yearMonth: '2021-07',
+        balance: 456.21
+      }
+      await balancesCollection.insertOne(balance1)
+      await balancesCollection.insertOne(balance2)
+      await balancesCollection.insertOne(balance3)
+      const count = await balancesCollection.countDocuments()
+      expect(count).toBe(3)
+
+      const balanceResult = await sut.loadLastBalance(accountId, '2021-07')
+
+      expect(balanceResult.id).toBeTruthy()
+      expect(balanceResult.yearMonth).toBe('2021-06')
+      expect(balanceResult.balance).toBe(321.22)
+      expect(balanceResult).not.toHaveProperty('accountId')
+    })
+
+    test('Should return null when there is no month before passed month parameter', async () => {
+      const sut = makeSut()
+      const accountId = await mockAccountId()
+      const balance1 = {
+        accountId,
+        yearMonth: '2021-05',
+        balance: 123.45
+      }
+      const balance2 = {
+        accountId,
+        yearMonth: '2021-06',
+        balance: 321.22
+      }
+      await balancesCollection.insertOne(balance1)
+      await balancesCollection.insertOne(balance2)
+      const count = await balancesCollection.countDocuments()
+      expect(count).toBe(2)
+
+      const balanceResult = await sut.loadLastBalance(accountId, '2021-05')
+
+      expect(balanceResult).toBeNull()
+    })
+  })
+
+  describe('loadFutureBalances()', () => {
+    test('Should load future balances from a month parameter', async () => {
+      const sut = makeSut()
+      const accountId = await mockAccountId()
+      const balance1 = {
+        accountId,
+        yearMonth: '2021-05',
+        balance: 123.45
+      }
+      const balance2 = {
+        accountId,
+        yearMonth: '2021-06',
+        balance: 321.22
+      }
+      const balance3 = {
+        accountId,
+        yearMonth: '2021-07',
+        balance: 456.21
+      }
+      await balancesCollection.insertOne(balance1)
+      await balancesCollection.insertOne(balance2)
+      await balancesCollection.insertOne(balance3)
+      const count = await balancesCollection.countDocuments()
+      expect(count).toBe(3)
+
+      const balanceResult = await sut.loadFutureBalances(accountId, '2021-05')
+
+      expect(balanceResult.length).toBe(2)
+
+      expect(balanceResult[0].id).toBeTruthy()
+      expect(balanceResult[0].yearMonth).toBe('2021-06')
+      expect(balanceResult[0].balance).toBe(321.22)
+      expect(balanceResult[0]).not.toHaveProperty('accountId')
+
+      expect(balanceResult[1].id).toBeTruthy()
+      expect(balanceResult[1].yearMonth).toBe('2021-07')
+      expect(balanceResult[1].balance).toBe(456.21)
+      expect(balanceResult[1]).not.toHaveProperty('accountId')
+    })
+
+    test('Should return empty array when there is no months after the month parameter', async () => {
+      const sut = makeSut()
+      const accountId = await mockAccountId()
+      const balance1 = {
+        accountId,
+        yearMonth: '2021-05',
+        balance: 123.45
+      }
+      const balance2 = {
+        accountId,
+        yearMonth: '2021-06',
+        balance: 321.22
+      }
+      await balancesCollection.insertOne(balance1)
+      await balancesCollection.insertOne(balance2)
+      const count = await balancesCollection.countDocuments()
+      expect(count).toBe(2)
+
+      const balanceResult = await sut.loadFutureBalances(accountId, '2021-06')
+
+      expect(balanceResult).toEqual([])
     })
   })
 })
