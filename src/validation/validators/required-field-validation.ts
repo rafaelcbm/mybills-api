82c28ad5af0bcab7ca41b7f352bf2d158a88fb0d
Rafael Correia <rafaelcbm@gmail.com>
2021-08-04 18:39:58 -0300

    fix: required fields validation

--- src/validation/validators/required-field-validation.ts
+++ src/validation/validators/required-field-validation.ts
@@ -5,7 +5,7 @@ export class RequiredFieldValidation implements Validation {
   constructor (private readonly fieldName: string) {}
 
   validate (input: any): Error {
-    if (!input[this.fieldName]) {
+    if (input[this.fieldName] === null || input[this.fieldName] === undefined) {
       return new MissingParamError(this.fieldName)
     }
   }
