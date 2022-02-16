716bcca6d5fcf152b09e6dda13ac54150fb4aca8
Rafael Correia <rafaelcbm@gmail.com>
2022-02-16 17:40:53 -0300

    feat: load bill by id

--- src/infra/db/mongodb/bill-mongo-repository.ts
+++ src/infra/db/mongodb/bill-mongo-repository.ts
@@ -1,23 +1,36 @@
-import { AddBillRepository, AddBillRepositoryParams, AddBillRepositoryResult, AddManyBillsRepository, LoadBillsByMonthRepositoryParams, LoadBillsByMonthRepositoryResult, LoadBillsByMonthRepository } from '@/data/protocols/db'
+import { AddBillRepository, AddBillRepositoryParams, AddBillRepositoryResult, AddManyBillsRepository, LoadBillsByMonthRepositoryParams, LoadBillsByMonthRepositoryResult, LoadBillsByMonthRepository, LoadBillByIdRepository, LoadBillsByIdRepositoryResult, LoadBillByIdRepositoryParams } from '@/data/protocols/db'
 import { AddBillParams } from '@/domain/usecases'
 import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'bson'
 import addMonths from 'date-fns/addMonths'
 import parse from 'date-fns/parse'
 
-export class BillMongoRepository implements AddBillRepository, AddManyBillsRepository, LoadBillsByMonthRepository {
-  async add (billParam: AddBillRepositoryParams): Promise<AddBillRepositoryResult> {
+export class BillMongoRepository implements AddBillRepository, AddManyBillsRepository, LoadBillsByMonthRepository, LoadBillByIdRepository {
+  async loadBillById(params: LoadBillByIdRepositoryParams): Promise<LoadBillsByIdRepositoryResult> {
+    const billCollection = await MongoHelper.getCollection('bills')
+
+    const result = await billCollection.findOne(
+      {
+        accountId: params.accountId,
+        _id: new ObjectId(params.id)
+      },
+      { projection: { accountId: 0 } })
+
+    return result ? MongoHelper.map(result) : result
+  }
+
+  async add(billParam: AddBillRepositoryParams): Promise<AddBillRepositoryResult> {
     const billCollection = await MongoHelper.getCollection('bills')
     const bill = await billCollection.insertOne(billParam)
     return MongoHelper.map(bill.ops[0])
   }
 
-  async addMany (billsParam: AddBillParams[]): Promise<void> {
+  async addMany(billsParam: AddBillParams[]): Promise<void> {
     const billCollection = await MongoHelper.getCollection('bills')
     await billCollection.insertMany(billsParam)
   }
 
-  async loadBills (params: LoadBillsByMonthRepositoryParams): Promise<LoadBillsByMonthRepositoryResult[]> {
+  async loadBills(params: LoadBillsByMonthRepositoryParams): Promise<LoadBillsByMonthRepositoryResult[]> {
     const billCollection = await MongoHelper.getCollection('bills')
 
     const pattern = 'yyyy-MM'
@@ -28,7 +41,7 @@ export class BillMongoRepository implements AddBillRepository, AddManyBillsRepos
     const query = { $and: [{ accountId: new ObjectId(params.accountId) }, { date: { $gte: startDate } }, { date: { $lt: endDate } }] }
     const sort = { date: 1 }
 
-    const result = await billCollection.find(query,{ projection: { accountId: 0 } }).sort(sort).toArray()
+    const result = await billCollection.find(query, { projection: { accountId: 0 } }).sort(sort).toArray()
 
     return MongoHelper.mapCollection(result)
   }
