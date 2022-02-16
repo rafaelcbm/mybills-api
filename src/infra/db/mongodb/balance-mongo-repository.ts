02ccc013bb2da84902915ae4610176083555240b
Rafael Correia <rafaelcbm@gmail.com>
2022-02-15 21:58:37 -0300

    feat: load balance by id repository implementation

--- src/infra/db/mongodb/balance-mongo-repository.ts
+++ src/infra/db/mongodb/balance-mongo-repository.ts
@@ -1,44 +1,57 @@
-import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult, LoadFutureBalancesRepository, LoadLastBalanceRepository, UpdateBalanceRepository } from '@/data/protocols/db'
+import { AddBalanceRepository, AddBalanceRepositoryParams, LoadBalanceByIdRepository, LoadBalanceByMonthRepository, LoadBalanceByMonthRepositoryParams, LoadBalanceByMonthRepositoryResult, LoadFutureBalancesRepository, LoadLastBalanceRepository, UpdateBalanceRepository, LoadBalanceByIdRepositoryParams } from '@/data/protocols/db'
 import { BalanceModel } from '@/domain/models'
 import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'mongodb'
 
 export class BalanceMongoRepository implements LoadBalanceByMonthRepository, AddBalanceRepository, UpdateBalanceRepository,
-                                      LoadLastBalanceRepository, LoadFutureBalancesRepository {
-  async loadBalance (params: LoadBalanceByMonthRepositoryParams): Promise<LoadBalanceByMonthRepositoryResult> {
+  LoadLastBalanceRepository, LoadFutureBalancesRepository, LoadBalanceByIdRepository {
+  async loadBalanceById(params: LoadBalanceByIdRepositoryParams): Promise<BalanceModel> {
     const billCollection = await MongoHelper.getCollection('balances')
 
     const result = await billCollection.findOne(
-      { yearMonth: params.yearMonth , accountId: params.accountId },
+      {
+        accountId: params.accountId,
+        _id: new ObjectId(params.id)
+      },
       { projection: { accountId: 0 } })
 
     return result ? MongoHelper.map(result) : result
   }
 
-  async add (balanceParam: AddBalanceRepositoryParams): Promise<void> {
+  async loadBalance(params: LoadBalanceByMonthRepositoryParams): Promise<LoadBalanceByMonthRepositoryResult> {
+    const billCollection = await MongoHelper.getCollection('balances')
+
+    const result = await billCollection.findOne(
+      { yearMonth: params.yearMonth, accountId: params.accountId },
+      { projection: { accountId: 0 } })
+
+    return result ? MongoHelper.map(result) : result
+  }
+
+  async add(balanceParam: AddBalanceRepositoryParams): Promise<void> {
     const balanceCollection = await MongoHelper.getCollection('balances')
     await balanceCollection.insertOne(balanceParam)
   }
 
-  async update (balanceId: string, balanceValue: number): Promise<BalanceModel> {
+  async update(balanceId: string, balanceValue: number): Promise<BalanceModel> {
     const balanceCollection = await MongoHelper.getCollection('balances')
     const updatedBalance = await balanceCollection.findOneAndUpdate(
       {
         _id: new ObjectId(balanceId)
       }, {
-        $set: {
-          balance: balanceValue
-        }
-      }, {
-        returnOriginal: false
-      })
+      $set: {
+        balance: balanceValue
+      }
+    }, {
+      returnOriginal: false
+    })
 
     if (updatedBalance.ok && updatedBalance.value) {
       return MongoHelper.map(updatedBalance.value)
     }
   }
 
-  async loadLastBalance (accountId: string, yearMonth: string): Promise<BalanceModel> {
+  async loadLastBalance(accountId: string, yearMonth: string): Promise<BalanceModel> {
     const billCollection = await MongoHelper.getCollection('balances')
 
     const result = await billCollection.findOne(
@@ -48,7 +61,7 @@ export class BalanceMongoRepository implements LoadBalanceByMonthRepository, Add
     return result ? MongoHelper.map(result) : result
   }
 
-  async loadFutureBalances (accountId: string, yearMonth: string): Promise<BalanceModel[]> {
+  async loadFutureBalances(accountId: string, yearMonth: string): Promise<BalanceModel[]> {
     const billCollection = await MongoHelper.getCollection('balances')
 
     const result = await billCollection.find(
