feab7cfa4f9362f4dfdbc7cf7928fda5f4790df0
Rafael Correia <rafaelcbm@gmail.com>
2022-02-17 20:36:55 -0300

    refactor: typo

--- src/presentation/controllers/bill/load-bills-by-month-controller.ts
+++ src/presentation/controllers/bill/load-bills-by-month-controller.ts
@@ -3,18 +3,18 @@ import { badRequest, ok } from '@/presentation/helpers'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadBillsByMonthController implements Controller {
-  constructor (
+  constructor(
     private readonly validation: Validation,
     private readonly loadBillsByMonth: LoadBillsByMonth
-  ) {}
+  ) { }
 
-  async handle (request: LoadBillsByMonthControllerRequest): Promise<HttpResponse> {
+  async handle(request: LoadBillsByMonthControllerRequest): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
-    const categories = await this.loadBillsByMonth.loadBills(request)
-    return ok(categories)
+    const bills = await this.loadBillsByMonth.loadBills(request)
+    return ok(bills)
   }
 }
 
